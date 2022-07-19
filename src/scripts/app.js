28d333e9590bff28cc6feb961c1df67ccf6b1989
MinhThang <Thanmn9403@gmail.com>
2022-07-19 22:08:20 +0700

    done frontend

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,24 @@
 var darkMode = document.querySelector(".darkmode")
+var h5s = document.querySelectorAll("h5")
+var h3s = document.querySelectorAll("h3")
+var circle = document.querySelector(".circle")
+var body = document.querySelector("body")
+var p = document.querySelector("p")
+circle.onclick = function(){
+    return turnRight()
+}
+function turnRight(){
+    circle.classList.toggle("right0")
+   
+    body.classList.toggle("bg")
+    h5s.forEach((h5)=>{
+        h5.classList.toggle("text-white")
+    })
+    h3s.forEach((h3)=>{
+        h3.classList.toggle("text-white")
+    })
+    p.classList.toggle("text-white")
+}
+
+
+
